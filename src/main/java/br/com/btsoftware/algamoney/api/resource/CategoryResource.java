094017e7581f8163b19b2a4224e7915b7db7b66f
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-09-25 16:33:30 -0300

    fazendo exception

--- src/main/java/br/com/btsoftware/algamoney/api/resource/CategoryResource.java
+++ src/main/java/br/com/btsoftware/algamoney/api/resource/CategoryResource.java
@@ -1,15 +1,19 @@
 package br.com.btsoftware.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.btsoftware.algamoney.api.model.Category;
 import br.com.btsoftware.algamoney.api.repository.CategoryRepository;
@@ -27,11 +31,21 @@ public class CategoryResource {
 
 		return categories;
 	}
-	
+
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void create(@RequestBody Category category) {
-		categoryRepository.save(category);
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category categorySave = categoryRepository.save(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(categorySave.getId())
+				.toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categorySave);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category category = categoryRepository.findOne(id);
+		return category != null ? ResponseEntity.ok(category) : ResponseEntity.notFound().build();
 	}
 
 }

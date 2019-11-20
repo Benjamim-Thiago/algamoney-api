27bd9d56909a5605a010152a98bfba549c982e0c
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-11-19 23:49:31 -0300

    listagem de pessoas

--- src/main/java/br/com/btsoftware/algamoney/api/repository/PersonRepository.java
+++ src/main/java/br/com/btsoftware/algamoney/api/repository/PersonRepository.java
@@ -1,9 +1,13 @@
 package br.com.btsoftware.algamoney.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.btsoftware.algamoney.api.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long>{
+
+	public Page<Person> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
 }
--- src/main/java/br/com/btsoftware/algamoney/api/resource/PersonResource.java
+++ src/main/java/br/com/btsoftware/algamoney/api/resource/PersonResource.java
@@ -1,12 +1,12 @@
 package br.com.btsoftware.algamoney.api.resource;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,14 +39,6 @@ public class PersonResource {
 
 	@Autowired
 	private PersonService personService;
-
-	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-	public List<Person> list() {
-		List<Person> people = personRepository.findAll();
-
-		return people;
-	}
 
 	@PostMapping
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
@@ -77,12 +70,18 @@ public class PersonResource {
 	public void remover(@PathVariable Long id) {
 		this.personRepository.delete(id);
 	}
-	
+
 	@PutMapping("/{id}/status")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
 	public void updateStatusPropertie(@PathVariable Long id, @RequestBody PersonUpdateStatusDTO personUpdateStatusDTO) {
 		personService.updateStatusPropertie(id, personUpdateStatusDTO.getStatus());
+	}
+
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
+	public Page<Person> findAll(@RequestParam(required = false, defaultValue = "%") String name, Pageable pageable) {
+		return personRepository.findByNameContainingIgnoreCase(name, pageable);
 	}
 
 }

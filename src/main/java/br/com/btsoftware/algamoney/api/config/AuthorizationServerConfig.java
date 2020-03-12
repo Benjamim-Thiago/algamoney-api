394467e37b68bcd4d866d1b4b5a8100e5d6464cc
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2020-03-12 01:38:54 -0300

    mudança na classe de autorização

--- src/main/java/br/com/btsoftware/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/btsoftware/algamoney/api/config/AuthorizationServerConfig.java
@@ -34,8 +34,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.secret("@ngul@r0")
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
-				//.accessTokenValiditySeconds(1800) // 30 minutos
-				.accessTokenValiditySeconds(15) 
+				//.accessTokenValiditySeconds(1800) // 30 minutos em testes
+				.accessTokenValiditySeconds(15) // em produção
 				.refreshTokenValiditySeconds(3600 * 24)
 				//.refreshTokenValiditySeconds(20)
 				
